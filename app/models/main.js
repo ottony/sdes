d0de575e9a2bcef4ea8348edb4bcd4cc3c2e92b1
Ottony Costa <ottony93@gmail.com>
2016-09-29 02:12:48 -0300

    Changes main function to check algorithm

--- app/models/main.js
+++ app/models/main.js
@@ -5,22 +5,13 @@ import _ from 'underscore';
 
 class Main {
   static teste() {
-    let binList = StringParser.toBin("Ottony");
+    let key = new Bin('1010000010');
+    let buffer = StringParser.toBin('Ottony|Lizza');
 
-    console.log( _.map(binList, (b) => { return b.toChar(); }).join('') );
-
-    let k1, k2;
-    [k1, k2] = SDes.keys(new Bin('1010000010'));
-
-    console.log('k1:');
-    console.log(k1.asString());
-    console.log('k2:');
-    console.log(k2.asString());
-    console.log('sbox:');
-    console.log(SDes.sbox(
-        new Bin('00101011')
-      ).asString()
-    );
+    let message = new Buffer(_.map(buffer, (b) => {
+      return parseInt(SDes.crypt(b, key).asString(), 2).toString(10);
+    }));
+    console.log(message.toString('binary'));
   }
 }
 
