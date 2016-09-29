23dcf20382e229b2079af46789b6055f4b8500b1
Ottony Costa <ottony93@gmail.com>
2016-09-29 02:12:07 -0300

    Removes unused arg to string_parser

--- app/models/string_parser.js
+++ app/models/string_parser.js
@@ -2,7 +2,7 @@ import Bin from './bin.js';
 import _ from 'underscore';
 
 class StringParser {
-  static toBin(raw, max = 10) {
+  static toBin(raw) {
     let buffer = new Buffer(raw)
 
     return _.map(buffer, (char) => {
